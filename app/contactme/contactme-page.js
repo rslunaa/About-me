43da12b0d1116be9a3517150ef6787f52cdeae9d
rslunaa <ricardoluna1700@icloud.com>
2023-10-18 16:54:02 -0500

    coomit

--- app/contactme/contactme-page.js
+++ app/contactme/contactme-page.js
@@ -1,8 +1,6 @@
-const { Frame } = require('@nativescript/core');
-
 exports.onGoBackTap = args => {
   const button = args.object;
   const page = button.page;
 
-  Frame.topmost().goBack();
+  topmost().goBack();
 };
--- app/main-page.js
+++ app/main-page.js
@@ -1,5 +1,5 @@
 import { createViewModel } from './main-view-model';
-import { Frame } from '@nativescript/core';
+import { topmost } from '@nativescript/core';
 
 export function onNavigatingTo(args) {
   const page = args.object;
@@ -10,26 +10,26 @@ exports.onAboutMeTap = args => {
   const button = args.object;
   const page = button.page;
 
-  page.frame.navigate('aboutme/aboutme-page');
+  topmost().navigate('aboutme/aboutme-page');
 };
 
 exports.onSkillsTap = args => {
   const button = args.object;
   const page = button.page;
 
-  page.frame.navigate('skills/skills-page');
+  topmost().navigate('skills/skills-page');
 };
 
 exports.onContactMeTap = args => {
   const button = args.object;
   const page = button.page;
 
-  page.frame.navigate('contactme/contactme-page');
+  topmost().navigate('contactme/contactme-page');
 };
 
 exports.onGoBackTap = args => {
   const button = args.object;
   const page = button.page;
 
-  page.frame.goBack();
+  topmost().goBack();
 };
