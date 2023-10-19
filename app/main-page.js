eb2185a6e223834da229aa2fee55ecc2d8fa708e
rslunaa <ricardoluna1700@icloud.com>
2023-10-18 19:07:37 -0500

    commit

--- app/main-page.js
+++ app/main-page.js
@@ -10,26 +10,26 @@ export function onAboutMeTap (args) {
   const button = args.object;
   const page = button.page;
 
-  topmost().navigate('aboutme/aboutme-page');
+  page.frame.navigate('aboutme/aboutme-page');
 };
 
 export function onSkillsTap (args) {
   const button = args.object;
   const page = button.page;
 
-  topmost().navigate('skills/skills-page');
+  page.frame.navigate('skills/skills-page');
 };
 
 export function onContactMeTap (args) {
   const button = args.object;
   const page = button.page;
 
-  topmost().navigate('contactme/contactme-page');
+  page.frame.navigate('contactme/contactme-page');
 };
 
 export function onGoBackTap (args) {
   const button = args.object;
   const page = button.page;
 
-  topmost().goBack('main-page');
+  page.frame.navigate('main-page');
 };
