3422b7599afd29e8b535b6b37ce07971edd2e131
rslunaa <ricardoluna1700@icloud.com>
2023-10-30 14:57:24 -0500

    Commit

--- app/skills/skills-page.js
+++ app/skills/skills-page.js
@@ -4,3 +4,28 @@ export function onGoBackTap (args) {
 
   page.frame.navigate('main-page');
 };
+
+// skills-page.js
+const observableModule = require("tns-core-modules/data/observable");
+
+function onNavigatingTo(args) {
+    const page = args.object;
+    const viewModel = observableModule.fromObject({
+        skills: [
+            { category: "Programming Skills", skill: "JAVA" },
+            { category: "Programming Skills", skill: "PYTHON" },
+            { category: "Programming Skills", skill: "C#" },
+            { category: "Programming Skills", skill: "Visual Basic" },
+            { category: "Cybersecurity Skills", skill: "Cybersecurity Fundamentals" },
+            { category: "Cybersecurity Skills", skill: "Network Security & Data Protection" },
+            { category: "Cybersecurity Skills", skill: "Cloud Security" },
+            { category: "Soft Skills", skill: "Collaboration and Teamwork" },
+            { category: "Soft Skills", skill: "Troubleshooting and Testing" },
+            { category: "Soft Skills", skill: "Problem Solving" },
+            { category: "Soft Skills", skill: "Critical Thinking" },
+        ],
+    });
+    page.bindingContext = viewModel;
+}
+
+exports.onNavigatingTo = onNavigatingTo;
