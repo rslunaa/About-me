6f1ab94a38dc72b2b8eef1905c2112b43deea172
rslunaa <ricardoluna1700@icloud.com>
2023-11-01 12:06:25 -0500

    Commit

--- app/contactme/contactme-page.js
+++ app/contactme/contactme-page.js
@@ -1,6 +1,12 @@
-export function onGoBackTap (args) {
+// contactme-page.js
+export function onGoBackTap(args) {
   const button = args.object;
   const page = button.page;
 
   page.frame.navigate('main-page');
-};
+}
+
+export function onSubmitTap(args) {
+  // For demonstration purposes, just show a success message
+  alert("Submission Successful");
+}
